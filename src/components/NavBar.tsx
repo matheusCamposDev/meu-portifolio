9a1e91cffb731d01ce703aebed3a4abef57cc17c
Matheus Campos <theuscampos45@gmail.com>
2025-07-02 10:37:43 -0300

    style: ajusta layout da navbar para melhor alinhamento e remoção do título

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,7 @@
 export function Navbar() {
   return (
     <header className="bg-gray-900 text-white fixed top-0 left-0 w-full shadow-md z-50">
-      <nav className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
-        <h1 className="text-xl font-bold">Matheus Campos</h1>
+      <nav className="mx-auto px-20 py-3 flex justify-end items-center">
         <ul className="flex gap-6">
           <li><a href="#home" className="hover:text-blue-400">Home</a></li>
           <li><a href="#sobre" className="hover:text-blue-400">Sobre mim</a></li>
