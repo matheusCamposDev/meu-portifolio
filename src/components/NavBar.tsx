989864b2ba5540306d26008d2fc6dfa93a2be97b
Matheus Campos <theuscampos45@gmail.com>
2025-07-03 08:52:24 -0300

    refactor: substituir tags <a> por <Link> para navegação SPA com React Router

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export function Navbar() {
   return (
     <header className="bg-gray-900 text-white fixed top-0 left-0 w-full shadow-md z-50">
       <nav className="mx-auto px-20 py-3 flex justify-end items-center">
         <ul className="flex gap-6">
-          <li><a href="/#/home" className="hover:text-blue-400">Home</a></li>
-          <li><a href="/#/about" className="hover:text-blue-400">Sobre mim</a></li>
-          <li><a href="/#/projects" className="hover:text-blue-400">Projetos</a></li>
-          <li><a href="/#/contact" className="hover:text-blue-400">Contato</a></li>
+          <li><Link to="/home" className="hover:text-blue-400">Home</Link></li>
+          <li><Link to="/about" className="hover:text-blue-400">Sobre mim</Link></li>
+          <li><Link to="/projects" className="hover:text-blue-400">Projetos</Link></li>
+          <li><Link to="/contact" className="hover:text-blue-400">Contato</Link></li>
         </ul>
       </nav>
     </header>
