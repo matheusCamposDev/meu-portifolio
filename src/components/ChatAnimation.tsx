c0133cd08614ec625ddf063febf04ee18d80b2a8
Matheus Campos <theuscampos45@gmail.com>
2025-07-02 23:32:16 -0300

    fix: corrigir caminho da imagem quebrada no componente ChatAnimation

--- src/components/ChatAnimation.tsx
+++ src/components/ChatAnimation.tsx
@@ -1,17 +1,18 @@
 import { TypeAnimation } from "react-type-animation";
+import devIcon from '../assets/icons8-desenvolvedor-64.png';
 
 export function ChatAnimation() {
   return (
     <>
     <img
-            src="src/assets/icons8-desenvolvedor-64.png"
+            src={devIcon}
             alt="foto de chat"
           />
     <div className="bg-gray-900 text-green-400 font-mono text-base md:text-base p-6 h-30 rounded-lg shadow-lg max-w-xl w-full text-start border border-green-600">
       <TypeAnimation
         style={{ whiteSpace: "pre-line", display: "block" }}
         sequence={[
-          `> Olá, sou o Matheus e sou Desenvolvedor FullStack\n> com 3 anos de experiência em desenvolvimento web \n > e alguns projetos em meu portfólio.`,
+          `> Oláa, sou o Matheus e sou Desenvolvedor FullStack\n> com 3 anos de experiência em desenvolvimento web \n > e alguns projetos em meu portfólio.`,
           1000,
         ]}
         wrapper="span"
