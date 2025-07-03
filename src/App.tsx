b7792a967f5d29af3ae11537e4c28ac2f1d3493f
Matheus Campos <theuscampos45@gmail.com>
2025-07-03 09:58:19 -0300

    fix: corrigir navegação inicial usando HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from "./components/NavBar";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { Home } from "./pages/Home";
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
           className="h-screen bg-black gap-8 flex flex-row items-center justify-center px-4"
         >
           <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />} />
             <Route path="/home" element={<Home />} />
           </Routes>
         </section>
