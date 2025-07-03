533eaab7dc82ca9c9cf4d205ecd43f479c7b91c5
Matheus Campos <theuscampos45@gmail.com>
2025-07-03 15:54:34 -0300

    style: ajusta responsividade da navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export function Navbar() {
   return (
     <header className="bg-gray-900 fixed top-0 left-0 w-full">
-      <nav className="mx-auto pr-20 py-3 flex justify-end items-center">
+      <nav className="mx-auto lg:pr-10 md:pr-10 py-3 flex justify-center lg:justify-end md:justify-end sm:justify-center items-center">
         <ul className="flex gap-6 text-white font-mono">
           <li><Link to="/home" className="hover:text-green-400">Home</Link></li>
           <li><Link to="/about" className="hover:text-green-400">Sobre mim</Link></li>
