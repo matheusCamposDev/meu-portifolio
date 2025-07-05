00967e4ff7237c5513a140bd3bd4adb95522c060
Matheus Campos <theuscampos45@gmail.com>
2025-07-05 20:46:32 -0300

    feat: Adicionar mensagem para página de projetos

---
+++ src/pages/Projetcts.tsx
@@ -0,0 +1,9 @@
+import { ChatAnimation } from "../components/ChatAnimation";
+
+export function Projects() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center gap-8 w-full">
+      <ChatAnimation sequence={[`> Está página ainda está em desenvolvimento e ainda não possui conteúdo.`, 1000]} />
+    </div>
+  );
+}
